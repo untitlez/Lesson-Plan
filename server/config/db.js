0d00f255490e4a04f2ff82d91f15358d08f8af51
untitlez <tleza_championship2@hotmail.com>
2025-12-29 23:18:45 +0700

    fix : error

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 
 exports.connectDB = async () => {
   try {
+    if (!process.env.DATABASE_URL) {
+      throw new Error("DATABASE_URL is not defined");
+    }
+
     await mongoose.connect(process.env.DATABASE_URL);
-  } catch {
-    process.exit(1);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    throw err;
   }
 };
